9aaaa7597194e815f5c214434e7e200192b5e838
Kazu <nokaha1222@gmail.com>
2022-05-03 21:21:38 +0900

    Initial Commit

---
+++ Shared/SampleToDoApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleToDoApp_SwiftUIApp.swift
+//  Shared
+//
+//  Created by 能勢航羽 on 2022/05/03.
+//
+
+import SwiftUI
+
+@main
+struct SampleToDoApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
